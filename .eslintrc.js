ac2bc5313db47074c445fb85001a2ccec086fc32
Seung-hyun Hwang <32220765+hsh2001@users.noreply.github.com>
2020-04-13 14:52:08 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-var': 'error',
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'getter-return': 'warn',
+    'no-extra-semi': 'warn',
+    'no-empty': 'warn',
+    'no-template-curly-in-string': 'warn',
+    'array-callback-return': 'error',
+    'dot-notation': 'error',
+    'no-throw-literal': 'error',
+    'prefer-regex-literals': 'error',
+    'no-warning-comments': 'warn',
+    'require-await': 'warn',
+    'object-shorthand': 'warn',
+    'no-unneeded-ternary': 'warn',
+    eqeqeq: ['error', 'smart'],
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all',
+      },
+    ],
+  },
+};
